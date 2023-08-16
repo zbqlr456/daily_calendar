de0668976b5cbd6101b081f15d9e7089a5f00e25
zbqlr456 <zbqlr456@naver.com>
2023-08-16 23:35:51 +0900

    Add Calendar

---
+++ client/lib/component/calendar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatefulWidget {
+  const Calendar({super.key});
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      focusedDay: DateTime.now(),
+      firstDay: DateTime(1900),
+      lastDay: DateTime(3000),
+    );
+  }
+}
