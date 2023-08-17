7748217f0b9299d1a2d7206a8c0ab9ae55b4579b
zbqlr456 <zbqlr456@naver.com>
2023-08-17 23:58:23 +0900

    Add daily_circle

---
+++ client/lib/component/daily_circle.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DailyCircle extends StatefulWidget {
+  const DailyCircle({super.key});
+
+  @override
+  State<DailyCircle> createState() => _DailyCircleState();
+}
+
+class _DailyCircleState extends State<DailyCircle> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
